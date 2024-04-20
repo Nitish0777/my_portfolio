6c4988bf8c93b9dec883ddecca332a5bd3551f2c
Nitish Nashine <nitishnashine@gmail.com>
2024-04-20 21:54:40 +0530

    work updated

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,24 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { AppWrap, MotionWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
 
 const Work = () => {
-  const [works, setWorks] = useState([]);
+  const [works, setWorks] = useState([
+    {
+      id: 1,
+      title: "E-commerce Project",
+      imgUrl: "../../assets/project1.png",
+      projectLink: "https://mern-store-gold.vercel.app/",
+      codeLink: "https://github.com/Nitish0777/e-com",
+      description:
+        "Our E-commerce Project is a user-friendly web platform built on the MERN stack, offering a diverse range of products with intuitive browsing, secure transactions, and seamless checkout. Explore a wide array of categories and shop with confidence.",
+      tags: ["Web App"],
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      imgUrl: "../../assets/project2.png",
+      projectLink: "https://konnect-dashboard-frontend.vercel.app/",
+      codeLink: "https://github.com/Nitish0777/konnect_dashboard",
+      description:
+        "Konnect Dashboard is a powerful tool built with the MERN stack and React JS for tracking user activities and analytics. Monitor user interactions, track performance metrics, and gain valuable insights for informed decision-making.",
+      tags: ["React JS"],
+    },
+  ]);
+
   const [filterWork, setFilterWork] = useState([]);
   const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
-    const query = '*[_type == "works"]';
-
-    client.fetch(query).then((data) => {
-      setWorks(data);
-      setFilterWork(data);
-    });
-  }, []);
+    setFilterWork(works);
+  }, [works]);
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
@@ -42,19 +59,17 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "MERN", "React JS", "All"].map(
-          (item, index) => (
-            <div
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
+        {["UI/UX", "Web App", "MERN", "React JS", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
 
       <motion.div
@@ -65,7 +80,7 @@ const Work = () => {
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <img src={work.imgUrl} alt={work.title} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
@@ -101,12 +116,19 @@ const Work = () => {
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10, textAlign:"center"}}>
+              <p
+                className="p-text"
+                style={{ marginTop: 10, textAlign: "center" }}
+              >
                 {work.description}
               </p>
 
               <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
+                {work.tags.map((tag, index) => (
+                  <p className="p-text" key={index}>
+                    {tag}
+                  </p>
+                ))}
               </div>
             </div>
           </div>
@@ -116,9 +138,5 @@ const Work = () => {
   );
 };
 
-export default AppWrap(
-  MotionWrap(Work, 'app__works'),
-  'work',
-  'app__bg',
-);
+export default AppWrap(MotionWrap(Work, "app__works"), "work", "app__bg");
 // export default AppWrap(Work, "work");
